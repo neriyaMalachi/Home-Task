fb975a74f90802047d8dd45052e2461bf2c2b99d
Neriya Malachi <neriamalachi@gmail.com>
2025-03-09 16:45:15 +0200

    fixed errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
+      <body className="text-right">
         <NavBar />
         {children}
       </body>
